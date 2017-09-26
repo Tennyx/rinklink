5e860605c116ff522e93512bc2a1a4f1b07bf4a9
AUDIO_1 <EDIT_04@AUDIO-1.local>
2017-09-26 16:00:09 -0400

    working with htmlcalendar

--- rink_calendar/views.py
+++ rink_calendar/views.py
@@ -3,13 +3,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 import datetime
+import calendar
 
 # Create your views here.
 def rink_calendar(request):
-	print(datetime.date(2017,8,17))
+	myCal = calendar.HTMLCalendar(calendar.SUNDAY)
+	print(type(myCal.formatmonth(2017, 7)))
 	data = {'month' : 'January',
 			'number_of_days': range(31),
-
-
-	}
+			'calendar': myCal.formatmonth(2017, 7)
+			}
 	return render(request, 'rink_calendar/rink-calendar.html', {'data': data})
