290573ed4082d01f2ab884a8ae9c084534142e6e
AUDIO_1 <EDIT_04@AUDIO-1.local>
2017-09-27 15:32:03 -0400

    adding jquery functionality.

--- rink_calendar/static/js/rinkCalendar.js
+++ rink_calendar/static/js/rinkCalendar.js
@@ -1,12 +1,50 @@
+
+let monthVar = 0;
+let startWeek = moment().startOf('month').add(monthVar, "month").week();
+let endWeek = moment().endOf('month').add(monthVar, "month").week();
+
+function createCal(){
+	let calendar = [];
+	let weeks = [];
+
+	for(var week = startWeek;week<endWeek+1;week++){
+  		calendar.push({
+    		week:week,
+    		days:Array(7).fill(0).map((n, i) => moment().week(week).startOf('week').clone().add(n + i, 'day'))
+  		})
+	}
+
+
+	for(var d=0;d<calendar.length;d++){
+		let daysInWeek = [];
+		for(var e=0;e<7;e++){
+			daysInWeek.push('<td className="cellShell">' + calendar[d].days[e].format("D") + '</td>');	
+		}
+		weeks.push('<tr>' + daysInWeek + '</tr>');
+	}
+	$('#rinkCal').empty();
+	$('#rinkCal').append(weeks);
+
+}
 $( document ).ready(function() {
-    
 
+	
 
+	
+	
+   	$('#fwd').click(function(){
+   		monthVar += 1;	
+   		startWeek = moment().startOf('month').add(monthVar, "month").week();
+   		endWeek = moment().endOf('month').add(monthVar, "month").week();
+   		createCal();
+   	});
 
-
-
-
-
+   	$('#back').click(function(){
+   		monthVar -= 1;	
+   		startWeek = moment().startOf('month').add(monthVar, "month").week();
+   		endWeek = moment().endOf('month').add(monthVar, "month").week();
+   		createCal();
+   	});
 
 
 
--- rink_calendar/views.py
+++ rink_calendar/views.py
@@ -1,16 +1,14 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-import datetime
+from datetime import datetime
 import calendar
 
-# Create your views here.
 def rink_calendar(request):
 	myCal = calendar.HTMLCalendar(calendar.SUNDAY)
-	print(type(myCal.formatmonth(2017, 7)))
-	data = {'month' : 'January',
-			'number_of_days': range(31),
+	today = datetime.today()
+	print(datetime(today.year,today.month, 1))
+	data = {
 			'calendar': myCal.formatmonth(2017, 7)
 			}
 	return render(request, 'rink_calendar/rink-calendar.html', {'data': data})
