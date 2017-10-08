7590563f55fa37d6324079745b1d1d57a2908a7d
Cale Switzer <Tennyx@Cales-MacBook-Pro.local>
2017-10-08 19:39:31 -0400

    modal adjustments.

--- rink_calendar/static/js/rinkCalendar.js
+++ rink_calendar/static/js/rinkCalendar.js
@@ -3,7 +3,7 @@ let monthVar = 0;
 let monthHeader = moment().startOf('month').add(monthVar, "month").format("MMMM YYYY");
 let calData = {
 				months: {},
-				events: {'October 2017': []}
+				events: {}
 			};
 
 
@@ -15,20 +15,18 @@ function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
-function drop(ev) {
+function drop(ev, el) {
     ev.preventDefault();
     let data = ev.dataTransfer.getData("text");
     if(document.getElementById(data).id[0] == 'e' || ev.altKey){
     	let nodeCopy = document.getElementById(data).cloneNode(true);	
     	nodeCopy.id = ev.target.id + nodeCopy.textContent;
-    	ev.target.appendChild(nodeCopy);
-    	console.log(ev.target.id);
-    	console.log(nodeCopy.textContent);
+    	el.appendChild(nodeCopy);
     }
     else{
     	let nodeInd = document.getElementById(data);
     	nodeInd.id = ev.target.id + nodeInd.textContent;
-    	ev.target.appendChild(nodeInd);	
+    	el.appendChild(nodeInd);	
     }
 }
 
@@ -50,7 +48,7 @@ function createCal(date){
 		}
 		else{
 			firstWeek.push(
-				'<td class="cellShell" ondrop="drop(event)" ondragover="allowDrop(event)" id="' + firstDays + '">\
+				'<td class="cellShell" ondrop="drop(event, this)" ondragover="allowDrop(event)" id="' + firstDays + '">\
 					<div class="dateNum">' + firstDays +'</div>\
 				</td>'
 			);
@@ -67,7 +65,7 @@ function createCal(date){
 		let daysInWeek = [];
 		for(i=0;i<7;i++){
 			daysInWeek.push(
-				'<td class="cellShell" ondrop="drop(event)" ondragover="allowDrop(event)" id=' + day + '>\
+				'<td class="cellShell" ondrop="drop(event, this)" ondragover="allowDrop(event)" id=' + day + '>\
 					<div class="dateNum">' + day +'</div>\
 				</td>'
 			);
@@ -98,6 +96,46 @@ function createCal(date){
 
 $( document ).ready(function() {
 
+	$('#eventDiv').append(
+		'<div class="modal fade" id="createEventMod" tabindex="-1" role="dialog" aria-labelledby="createEventLabel" aria-hidden="true">\
+			<div class="modal-dialog" role="document">\
+				<div class="modal-content">\
+					<div class="modal-header">\
+						<h5 class="modal-title" id="createEventLabel">New Event</h5>\
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">\
+							<span aria-hidden="true">&times;</span>\
+						</button>\
+					</div>\
+					<div class="modal-body">\
+		        	<form>\
+	          			<div class="form-group">\
+	            			<label for="rinkEvent" class="form-control-label">Event Title:</label>\
+	            			<input type="text" class="form-control" id="rinkEvent">\
+	          			</div>\
+	          			<div class="form-group">\
+	            			<label for="eventTime" class="form-control-label">Time:</label>\
+	            			<input type="text" class="form-control" id="eventTime">\
+	          			</div>\
+	          			<div class="form-group">\
+	            			<label for="eventPrice" class="form-control-label">Price:</label>\
+	            			<input type="text" class="form-control" id="eventPrice">\
+	          			</div>\
+	         			<div class="form-group">\
+	            			<label for="eventDesc" class="form-control-label">Description:</label>\
+	            			<textarea class="form-control" id="eventDesc"></textarea>\
+	          			</div>\
+	        		</form>\
+					</div>\
+					<div class="modal-footer">\
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>\
+						<button id="saveNewEvent" type="button" class="btn btn-primary" data-dismiss="modal">Save changes</button>\
+					</div>\
+				</div>\
+			</div>\
+		</div>'
+		
+		);
+
 	createCal();
 	
 	$('#fwd').click(function(){
@@ -119,60 +157,83 @@ $( document ).ready(function() {
    	});
 
    	$('#rinkCal').on('click','.cellData',function(){
-   		$modal.modal('show');
+   		// $modal.modal('show');
    	});
 
    	$('#eventDiv').on('click','.cellData',function(){
-   		$modal.modal('show');
-   	});
-
-   	$('.saveBtn').click(function(){
-
-   	});
-
-   	$('#createEvent').click(function(){
-   		calData.events[monthHeader].push('x');
    		console.log(calData);
+   		console.log(this.id);
+   		console.log(calData.events[this.id].title);
    		$('#eventDiv').append(
-
-   	'<div id="eventdrag1" class="cellData" draggable="true" ondragstart="drag(event)" data-toggle="modal" data-target="#myModal">Untitled Event</div>\
-   	\
-   	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">\
-		<div class="modal-dialog" role="document">\
-			<div class="modal-content">\
-				<div class="modal-header">\
-					<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>\
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">\
-						<span aria-hidden="true">&times;</span>\
-					</button>\
+   			'<div class="modal fade" id="eventModal" tabindex="-1" role="dialog" aria-labelledby="eventModalLabel" aria-hidden="true">\
+				<div class="modal-dialog" role="document">\
+					<div class="modal-content">\
+						<div class="modal-header">\
+							<h5 class="modal-title" id="eventModalLabel">Modal title</h5>\
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">\
+							<span aria-hidden="true">&times;</span>\
+							</button>\
+						</div>\
+						<div class="modal-body">\
+				        	<form>\
+			          			<div class="form-group">\
+			            			<label for="rinkEvent" class="form-control-label">Event Title:</label>\
+			            			<input type="text" value="' + calData.events[this.id].title + '" class="form-control" id="rinkEvent">\
+			          			</div>\
+			          			<div class="form-group">\
+			            			<label for="eventTime" class="form-control-label">Time:</label>\
+			            			<input type="text" value="' + calData.events[this.id].time + '" class="form-control" id="eventTime">\
+			          			</div>\
+			          			<div class="form-group">\
+			            			<label for="eventPrice" class="form-control-label">Price:</label>\
+			            			<input type="text" value="' + calData.events[this.id].price + '" class="form-control" id="eventPrice">\
+			          			</div>\
+			         			<div class="form-group">\
+			            			<label for="eventDesc" class="form-control-label">Description:</label>\
+			            			<textarea class="form-control" id="eventDesc">' + calData.events[this.id].desc + '</textarea>\
+			          			</div>\
+			        		</form>\
+						</div>\
+						<div class="modal-footer">\
+							<button type="button" class="saveBtn btn btn-primary">SAVE</button>\
+						</div>\
+					</div>\
 				</div>\
-				<div class="modal-body">\
-	        	<form>\
-          			<div class="form-group">\
-            			<label for="rinkEvent" class="form-control-label">Event Title:</label>\
-            			<input type="text" class="form-control" id="rinkEvent">\
-          			</div>\
-          			<div class="form-group">\
-            			<label for="eventTime" class="form-control-label">Time:</label>\
-            			<input type="text" class="form-control" id="eventTime">\
-          			</div>\
-          			<div class="form-group">\
-            			<label for="eventPrice" class="form-control-label">Price:</label>\
-            			<input type="text" class="form-control" id="eventPrice">\
-          			</div>\
-         			<div class="form-group">\
-            			<label for="eventDesc" class="form-control-label">Description:</label>\
-            			<textarea class="form-control" id="eventDesc"></textarea>\
-          			</div>\
-        		</form>\
-				</div>\
-				<div class="modal-footer">\
-					<button type="button" class="saveBtn btn btn-primary">SAVE</button>\
-				</div>\
-			</div>\
-		</div>\
-	</div>');
-
+			</div>');
+   		$('#eventModal').on('hidden.bs.modal', function () {
+   			$('#eventModal').remove();
+   		});
+   		// $modal.modal('show');
    	});
+
+   	// $('.saveBtn').click(function(){
+
+   	// });
+
+
+
+   	$('#saveNewEvent').click(function(){
+   		let eventId = $('#rinkEvent').val() + $('#eventTime').val() + $('#eventPrice').val();
+
+   		if(eventId in calData){
+   			alert('Event with same title, time & price already exist.');
+   			return;
+   		}
+   		else{
+   			calData.events[eventId] = {};
+   			let eventTitle = calData.events[eventId]['title'] = $('#rinkEvent').val();
+   			let eventTime = calData.events[eventId]['time'] = $('#eventTime').val();
+   			let eventPrice = calData.events[eventId]['price'] = $('#eventPrice').val();
+   			let eventDesc = calData.events[eventId]['desc'] = $('#eventDesc').val();
+   			console.log(calData);
+	   		$('#eventDiv').append(
+				'<div id="' + eventId + '" class="cellData" draggable="true" ondragstart="drag(event)" data-toggle="modal" data-target="#eventModal">' + eventTitle + '</div>'
+			 );
+	   	}
+	   	
+	   	$('#createEventMod').on('hidden.bs.modal', function () {
+    		$(this).find("input,textarea,select").val('').end();
+		});
+	});
 
 });
